37fc8daa1655154208ea0fefece88a2705452799
Shivansh Tandon <76089733+shivanshtanonwork@users.noreply.github.com>
2025-12-25 10:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (97.19%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let x = 0;
-    for(let i = 0; i< nums.length;i++){
-        if(nums[i] != val){
-            nums[x] = nums[i];
-            x = x+1;
+    let x = 0
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i]!== val){
+            nums[x] = nums[i]
+            x = x + 1
         }
     }
-            return x;
+        return x
 };
