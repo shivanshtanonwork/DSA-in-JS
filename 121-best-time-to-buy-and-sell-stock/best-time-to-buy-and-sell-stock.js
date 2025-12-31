f92b6c1e57640e93d793af7ec2b2143f760718d0
Shivansh Tandon <76089733+shivanshtanonwork@users.noreply.github.com>
2025-12-31 13:39:28 +0530

    Time: 2 ms (81.18%) | Memory: 65.6 MB (57.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -4,15 +4,15 @@
  */
 var maxProfit = function(prices) {
     let min = prices[0];
-    let maxP = 0;
+    let maxProfit = 0;
 
-    for(let i=1; i<prices.length;i++){
-        if(prices[i]-min > maxP){
-            maxP = prices[i]-min;
+    for(let i = 1;i<prices.length;i++){
+        if(prices[i]-min > maxProfit){
+            maxProfit = prices[i] - min
         }
         if(prices[i]<min){
             min = prices[i]
         }
     }
-    return maxP;
+        return maxProfit;
 };
