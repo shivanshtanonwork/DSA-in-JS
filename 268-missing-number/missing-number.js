609f79e2f25f3ffdd300f5c2f82757b7251b03ae
Shivansh Tandon <76089733+shivanshtanonwork@users.noreply.github.com>
2026-01-04 11:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (86.96%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length
+    let totalSum = n * ( n + 1 ) / 2;
+    let partialSum = 0
+
+    for(let i = 0;i < n; i++){
+        partialSum = partialSum + nums[i]
+    }
+    return totalSum - partialSum
+};
