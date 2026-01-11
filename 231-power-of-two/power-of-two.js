6742ac10a9bf17cfb3037e930461d802c8148d80
Shivansh Tandon <76089733+shivanshtanonwork@users.noreply.github.com>
2026-01-11 10:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (7.33%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,8 +3,12 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if(n == 1) return true;
-    else if(n < 1 || n % 2 != 0) return false
+    // using recursion
+    // if(n == 1) return true;
+    // else if(n < 1 || n % 2 != 0) return false
 
-    return isPowerOfTwo(n/2)
+    // return isPowerOfTwo(n/2)
+
+    // using bitwise AND
+    return n > 0 && (n & (n - 1)) === 0
 };
